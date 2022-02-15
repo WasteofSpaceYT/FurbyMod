8ab83c987d0433dc088ef7e86b8340ba7c7d7bbb
WasteofSpaceYT <82965910+WasteofSpaceYT@users.noreply.github.com>
2022-02-15 17:52:08 -0500

    fix

--- src/main/java/com/waste/furbymod/entity/ModEntityTypes.java
+++ src/main/java/com/waste/furbymod/entity/ModEntityTypes.java
@@ -13,7 +13,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModEntityTypes {
     public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, FurbyMod.MOD_ID);
 
-    public static RegistryObject<EntityType<FurbyEntity>> Furby = ENTITY_TYPES.register("furby_entity", () -> EntityType.Builder.create(furbyentity::new, EntityClassification.CREATURE).size(1f,3f).build(new ResourceLocation(FurbyMod.MOD_ID, "furby_entity").toString()));
+    public static RegistryObject<EntityType<FurbyEntity>> Furby = ENTITY_TYPES.register("furby_entity", () -> EntityType.Builder.create(FurbyEntity::new, EntityClassification.CREATURE).size(1f,3f).build(new ResourceLocation(FurbyMod.MOD_ID, "furby_entity").toString()));
 
     public static void register(IEventBus eventBus){
         ENTITY_TYPES.register(eventBus);
